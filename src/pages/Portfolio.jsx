f9732e6545f06df667d267b58c9856d8c52cfa2e
Onosereme Emuemhonjie <eonosereme@gmail.com>
2023-02-20 23:01:38 +0100

    portfolio updated with latest projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,8 @@
+import countries from "../assets/countries.png";
+import form from "../assets/multistep-form.png";
 import todo from "../assets/todo.png";
 import ecommerce from "../assets/ecommerce.png";
 import notification from "../assets/notificatio.png";
-import portf from "../assets/portf.png";
 import tradelane from "../assets/tradelane.png";
 import essentialgroup from "../assets/essentialgroup.png";
 import { useGlobalContext } from "../Context";
@@ -16,52 +17,60 @@ const Portfolio = () => {
       <h3 className='h3'>A couple projects I have worked on</h3>
       <div className='portfolio-grid'>
         <a
+          href='https://multi-form-wizard.netlify.app/'
+          className='portfolio-grid-el full-width'
+          target='_blank'
+        >
+          <img src={form} alt='MULTI-STEP-FORM' />
+          <p>Form Wizard</p>
+        </a>
+        <a
           href='https://tradelane.netlify.app'
-          className='portfolio-grid-el portfolio-grid-el-five'
+          className='portfolio-grid-el '
           target='_blank'
         >
           <img src={tradelane} alt='TRADELANE' />
           <p>TradeLane</p>
         </a>
         <a
+          href='https://my-country-explorer.netlify.app/'
+          className='portfolio-grid-el '
+          target='_blank'
+        >
+          <img src={countries} alt='COUNTRY EXPLORER' />
+          <p>Country Explorer</p>
+        </a>
+        <a
           href='https://essentialgroup.netlify.app'
-          className='portfolio-grid-el portfolio-grid-el-six'
+          className='portfolio-grid-el full-width'
           target='_blank'
         >
           <img src={essentialgroup} alt='ESSENTIAL GROUP' />
           <p>EssentialGroup</p>
         </a>
         <a
-          href='https://onoseremejohn.github.io/todo-app'
-          className='portfolio-grid-el portfolio-grid-el-one'
+          href='https://onoseremejohn.github.io/todo-app/'
+          className='portfolio-grid-el '
           target='_blank'
         >
           <img src={todo} alt='TODO-APP' />
           <p>Todo-App</p>
         </a>
         <a
-          href='https://onoseremejohn.github.io/ecommerce'
+          href='https://onoseremejohn.github.io/Ecommerce-Product-Page/'
           target='_blank'
-          className='portfolio-grid-el portfolio-grid-el-two'
+          className='portfolio-grid-el '
         >
           <img src={ecommerce} alt='ECOMMERCE PRODUCT LANDING PAGE' />
           <p>ECommerce Product Page</p>
         </a>
         <a
-          href='https://onoseremejohn.github.io/notification'
+          href='https://onoseremejohn.github.io/notifications-page/'
           target='_blank'
-          className='portfolio-grid-el portfolio-grid-el-three'
+          className='portfolio-grid-el full-width last'
         >
           <img src={notification} alt='NOTIFICATIONS' />
           <p>Notifications Page</p>
-        </a>
-        <a
-          href='https://onoseremejohn.github.io/coursera-test/portfolio/index.html'
-          target='_blank'
-          className='portfolio-grid-el portfolio-grid-el-four'
-        >
-          <img src={portf} alt='lorem ipsum portfolio site' />
-          <p>Lorem Ipsum Portfolio Site</p>
         </a>
       </div>
     </section>
